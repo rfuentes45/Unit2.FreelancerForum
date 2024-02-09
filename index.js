2883b1eb5d290b1045fa390d3913be8481a3735d
rfuentes45 <156115769+rfuentes45@users.noreply.github.com>
2024-02-09 00:53:01 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+// first thing i did based on the guidelines from the workshop is make an array
+// based on the user story 
+
+const freelancers = [
+    {Name: 'Alice', Occupation: 'Writer', StartingP: 30},
+    {Name: 'Bob', Occupation: 'Teacher', StartingP: 50},
+    {Name: 'Carol', Occupation: 'Programmer', StartingP: 70}
+]
+
+// USER FINDS A MESSAGE THAT DISPLAYS THE AVERAGE STARTING PRICE OF $40
+
+//prompt('The starting price for all freelancers is $40')
+
+//const freelance = document.createElement('table')
+
+//const elements = document.querySelector(freelancers)
